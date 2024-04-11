1924256de21edf97b54cf7d4d18bb5140d4afd75
Televizzor <vizzor27@gmail.com>
2024-04-11 15:08:27 +0300

    Сделал отдельный класс для определения правильного падежа слово "Рубль" Так же отдельный класс для хранения информации о наименовании товара и его стоимости

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,119 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
 
+class RubFormat{
+    int rub;
+
+    String formatter(int rub){
+
+        this.rub = rub;
+        String ruble = "";
+        switch (rub){
+            case 1:
+                ruble = "Рубль";
+                break;
+            case 2:
+            case 3:
+            case 4:
+                ruble = "Рубля";
+                break;
+            case 0:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+                ruble = "Рублей";
+                break;
+        }
+        return ruble;
+
+    }
+}
+
+
+class Product{
+    String name;
+    float price;
+
+    Product(String name,float price){
+        this.name = name;
+        this.price = price;
+    }
+
+}
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<Product> products = new ArrayList<>();
+        System.out.println("Введите количество гостей среди которых нужно разделить счёт");
+        int peoples = 0;
+        float sum = 0;
+        while (true){
+            if (sc.hasNextInt()) {
+                peoples = sc.nextInt();
+                if (peoples < 1) {
+                    System.out.println("Введите правильное количество гостей");
+                } else if (peoples == 1){
+                    System.out.println("Делить не нужно, конец программы");
+                    return;
+                } else {
+                    System.out.println("Количество гостей: " + peoples);
+                    break;
+                }
+            } else {
+                System.out.println("Введите целое число гостей");
+                sc.next();
+            }
+
+        }
+        sc.nextLine();
+        while(true){
+            System.out.println("Введите название товара или 'Завершить' для отмены");
+            String name = sc.nextLine();
+
+            if (name.equalsIgnoreCase("Завершить")) {
+                break;
+            }
+            System.out.println("Введите цену товара");
+            while(true){
+
+                if(sc.hasNextFloat()){
+                    float price = sc.nextFloat();
+                    sc.nextLine();
+                    if (price > 0){
+                        products.add(new Product(name,price));
+                        sum += price;
+                        System.out.println("Товар " + name + " добавлен со стоимостью " + price);
+                        break;
+                    } else System.out.println("Введите корректную цену товара");
+
+                } else {
+                    System.out.println("Введите число");
+                    sc.next();
+                }
+
+
+            }
+
+        }
+        System.out.println("Добавленные товары: ");
+        for (int i = 0; i < products.size(); i++){
+            System.out.println("Товар: " + products.get(i).name + " стоимостью: " + products.get(i).price);
+        }
+        RubFormat rubFormat = new RubFormat();
+        float totalSum = sum/(float)peoples;
+        int rub = (int) totalSum % 10;
+        String ruble = rubFormat.formatter(rub);
+
+        System.out.print(String.format("Каждый гость должен заплатить: %.2f %s",totalSum, ruble ));
+
+
+
+
+
     }
 }
